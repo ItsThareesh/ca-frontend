991034e2503a82b36d070468d7e5e1a984b5cda9
Prabhu-E-S <elanprabhu009@gmail.com>
2026-09-15 12:05:26 +0530

    Masked the banner with the background and Enhanced the banner to the Asteria Theme

--- components/homepage/Hero.js
+++ components/homepage/Hero.js
@@ -14,8 +14,8 @@ export default function Hero() {
 			{/* Vacancy Banner floating at bottom center */}
 			<div className='vacancy-banner-container'>
 				<div className='vacancy-banner'>
-					<span className='vacancy-number'>6 7</span>
-					<span className='vacancy-label'>SEATS LEFT</span>
+					<span className='vacancy-number'>7</span>
+					<span className='vacancy-label'>DAYS LEFT</span>
 				</div>
 			</div>
 
