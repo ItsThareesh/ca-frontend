0a857cddf413b8d8f00803316b23f7cd3dca822d
mathew <amonline2005@gmail.com>
2026-09-21 13:04:24 +0530

    changed grp link

--- pages/profile.js
+++ pages/profile.js
@@ -127,8 +127,7 @@ const REFERRAL_BASE_URL = 'https://ca.tathva.org/?ref='
    swap in a new invite URL when the group is rotated. Left empty, the button
    simply isn't rendered. */
 const CA_WHATSAPP_GROUP_URL =
-	'https://chat.whatsapp.com/DFs33QxuGVR7rEg38ka5rg?s=cl&p=i&mlu=4&ilr=4'
-/* One-time nudge towards the WhatsApp group. It pops up shortly after the
+	'https://chat.whatsapp.com/D3bhWv9kAv5KYNwd2OrODI?s=sw&p=i&mlu=4&ilr=4' /* One-time nudge towards the WhatsApp group. It pops up shortly after the
    profile loads and closes itself again after a few seconds; "Don't show
    again" persists the opt-out per browser so it never reappears. */
 const WHATSAPP_POPUP_STORAGE_KEY = 'tathva_ca_whatsapp_popup_dismissed'
