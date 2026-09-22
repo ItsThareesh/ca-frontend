3461d1492a29312ff349cc638142b36812c17992
simonM21 <simonmattekkatt21@gmail.com>
2026-09-23 00:17:16 +0530

    logout in reg closed

--- components/regclosed/RegisterClosed.js
+++ components/regclosed/RegisterClosed.js
@@ -1,12 +1,20 @@
+import { useUserContext } from 'context/UserContext'
 import styles from './register-closed.module.css'
 
 export default function RegisterClosed() {
+	const { isLoggedIn, logout } = useUserContext()
+
 	return (
 		<>
 			<div className='container'>
 				<div className={styles['closed-container']}>
 					<p className={styles['regclosed-header']}>Registration Closed</p>
 					<p className={styles['regclosed-info']}>Contact us for any queries</p>
+					{isLoggedIn && (
+						<button type='button' className={styles['regclosed-logout']} onClick={logout}>
+							Sign out
+						</button>
+					)}
 				</div>
 				<div className='spacerv-md'></div>
 				<div className='spacerv-sm'></div>
